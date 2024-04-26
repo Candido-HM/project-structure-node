660b04fd6c14396f08425bc698991fc3e2421294
Candido Hernandez Martin <124414728+Candido-HM@users.noreply.github.com>
2024-04-25 18:04:33 -0600

    update src

--- src/app.js
+++ src/app.js
@@ -2,24 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-// const db = require('./models/index');
-// const orm = require('./models');
 
-//Ruta para cargar archivos de rutas
+//Ruta para cargar el archivo de rutas
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-// const PORT = process.env.PORT || 3000;
-
-// try {
-//     app.listen(PORT, () => {
-//         console.log(`Servidor iniciado en el puerto ${PORT}`);
-//     });
-// } catch (error) {
-//     console.log(`Error al iniciar el servidor: ${error.message}`);
-// }
-
-// app.use('/api', project_routes)
 
 module.exports = app;
